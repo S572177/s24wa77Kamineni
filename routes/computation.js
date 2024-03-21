22812d3927ae6fe03f9cd6a14338c6029507a8b6
S572177 <swethakamineni93@gmail.com>
2024-03-20 23:02:54 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -4,22 +4,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var x = (Math.random(1, 10));
-    var y = (Math.random(1, 10));
     console.log(x);
-    console.log(y);
 
     if (req.query.x){
         x = (req.query.x);
     }
-    if (req.query.y){
-        y = (req.query.y);
+    
+    function asinh(x){
+        return Math.asinh(x);
     }
-
-    function getCircleX(radians, radius) {
-        return Math.cos(radians) * radius;
-      }
-      
-      
+    console.log(asinh(x));
+    res.send(`Math.asinh applied on ${x} is ${asinh(x)}`);
 });
 
 module.exports = router;
